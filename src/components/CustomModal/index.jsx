4e72458d9d3fbfbb5befe3562195917dbada8e05
usama1095854 <https://github.com/usama1095854>
2025-03-25 03:37:48 +0500

    modal added

---
+++ src/components/CustomModal/index.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect } from 'react';
+import { Modal } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faTimes,
+  faQuestionCircle,
+  faCheckCircle,
+} from '@fortawesome/free-solid-svg-icons';
+
+import CustomButton from '../CustomButton';
+
+import './style.css';
+import { faExclamation } from '@fortawesome/free-solid-svg-icons/faExclamation';
+
+const CustomModal = (props) => {
+  useEffect(() => {
+    let timer;
+    if (props.show && props.autoClose) {
+      timer = setTimeout(() => {
+        props.close();
+      }, 1000); // Close the modal after 1 second
+    }
+    return () => clearTimeout(timer); // Clear timeout if modal closes before 1 second
+  }, [props.show, props.close]);
+
+  return (
+    <>
+      <Modal
+        show={props?.show}
+        className='customModal'
+        centered
+        onHide={props?.close}
+      >
+        <button className='closeButton' onClick={props?.close}>
+          <FontAwesomeIcon icon={faTimes} />
+        </button>
+        <Modal.Body className={props.children ? '' : 'text-center'}>
+          {props?.children ? (
+            ''
+          ) : props?.success ? (
+            <FontAwesomeIcon icon={faCheckCircle} className='checkMark' />
+          ) : (
+            <FontAwesomeIcon icon={faExclamation} className='questionMark' />
+          )}
+
+          <div className='modalContent'>
+            <h2 className='modalHeading text-center text-capitalize'>
+              {props?.heading}
+            </h2>
+            {props?.children ? (
+              <p>
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                  }}
+                  className='formDataStyle'
+                >
+                  {props?.children}
+                </form>
+              </p>
+            ) : props?.success ? null : (
+              <>
+                <CustomButton
+                  onClick={props?.action}
+                  variant='primaryButton'
+                  text='Accept'
+                  className='me-2'
+                />
+                <CustomButton
+                  onClick={props?.close}
+                  variant='secondaryButton'
+                  text='Reject'
+                />
+              </>
+            )}
+          </div>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+export default CustomModal;
--- src/screens/Auth/Login.jsx
+++ src/screens/Auth/Login.jsx
@@ -1,41 +1,45 @@
-import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
-import loginScreenBg from "../../assets/images/login-screen-bg.png";
+import loginScreenBg from '../../assets/images/login-screen-bg.png';
 
-import userIcon from "../../assets/images/user-icon.png";
-import emailIcon from "../../assets/images/email-icon.png";
-import passwordIcon from "../../assets/images/password-icon.png";
+import userIcon from '../../assets/images/user-icon.png';
+import emailIcon from '../../assets/images/email-icon.png';
+import passwordIcon from '../../assets/images/password-icon.png';
 
-import DefaultLayout from "../../components/DefaultLayout";
-import GlassBox from "../../components/GlassBox";
-import { Link, useNavigate } from "react-router-dom";
-import FullScreenBG from "../../components/FullScreenBG";
-import { LoginService } from "../../api/services/Auth";
-import { toast } from "react-toastify";
+import DefaultLayout from '../../components/DefaultLayout';
+import GlassBox from '../../components/GlassBox';
+import { Link, useNavigate } from 'react-router-dom';
+import FullScreenBG from '../../components/FullScreenBG';
+import { LoginService } from '../../api/services/Auth';
+import { toast } from 'react-toastify';
+import CustomModal from '../../components/CustomModal';
 
 const Login = () => {
   const [loading, setLoading] = useState(false);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  // const [showModal, setShowModal] = useState(true);
+  // const challengerName = 'John Doe';
+
   const navigate = useNavigate();
   const submitLogin = async () => {
     try {
       setLoading(true);
 
-      if (username !== "" && password !== "") {
+      if (username !== '' && password !== '') {
         const data = {
           username,
           password,
         };
         const response = await LoginService(data);
-        localStorage.setItem("token", response?.token);
-        localStorage.setItem("user",JSON.stringify( response?.data));
-        navigate("/context-entry");
+        localStorage.setItem('token', response?.token);
+        localStorage.setItem('user', JSON.stringify(response?.data));
+        navigate('/context-entry');
         setLoading(false);
       } else {
-        toast.error("User name and password both required");
+        toast.error('User name and password both required');
         setLoading(false);
       }
     } catch (error) {
@@ -45,40 +49,40 @@ const Login = () => {
   };
 
   return loading ? (
-    <div className="d-flex flex-column" style={{ minHeight: "100vh" }}>
+    <div className='d-flex flex-column' style={{ minHeight: '100vh' }}>
       <FullScreenBG backgroundImage={loginScreenBg}>
-        <h1 className="login-title">Loading.....</h1>
-        <div className="spinner-border text-primary" role="status">
-          <span className="visually-hidden">Loading...</span>
+        <h1 className='login-title'>Loading.....</h1>
+        <div className='spinner-border text-primary' role='status'>
+          <span className='visually-hidden'>Loading...</span>
         </div>
       </FullScreenBG>
     </div>
   ) : (
     <DefaultLayout>
       <FullScreenBG backgroundImage={loginScreenBg}>
-        <div className="col-xxl-5 col-xl-6 col-lg-7 col-md-9">
+        <div className='col-xxl-5 col-xl-6 col-lg-7 col-md-9'>
           <GlassBox>
-            <h2 className="login-title">Login</h2>
+            <h2 className='login-title'>Login</h2>
             <form>
-              <InputGroup className="mb-md-5 mb-4">
+              <InputGroup className='mb-md-5 mb-4'>
                 <InputGroup.Text>
-                  <img src={userIcon} alt="Icon" />
+                  <img src={userIcon} alt='Icon' />
                 </InputGroup.Text>
                 <Form.Control
-                  placeholder="Username"
+                  placeholder='Username'
                   value={username}
                   onChange={(e) => {
                     setUsername(e.target.value);
                   }}
                 />
               </InputGroup>
-              <InputGroup className="mb-2">
+              <InputGroup className='mb-2'>
                 <InputGroup.Text>
-                  <img src={passwordIcon} alt="Icon" />
+                  <img src={passwordIcon} alt='Icon' />
                 </InputGroup.Text>
                 <Form.Control
-                  type="password"
-                  placeholder="Password"
+                  type='password'
+                  placeholder='Password'
                   value={password}
                   onChange={(e) => {
                     setPassword(e.target.value);
@@ -86,30 +90,30 @@ const Login = () => {
                 />
               </InputGroup>
 
-              <div className="forget-pass-div d-flex justify-content-between align-items-center mb-4">
-                <div className="d-flex align-items-center">
-                  <input type="checkbox" id="rememberMe" className="me-2" />
-                  <label htmlFor="rememberMe" className="text-light">
+              <div className='forget-pass-div d-flex justify-content-between align-items-center mb-4'>
+                <div className='d-flex align-items-center'>
+                  <input type='checkbox' id='rememberMe' className='me-2' />
+                  <label htmlFor='rememberMe' className='text-light'>
                     Remember Me
                   </label>
                 </div>
 
                 {/* Forget Password Link */}
-                <Link to="javascript:;" className="forget-password-btn">
+                <Link to='javascript:;' className='forget-password-btn'>
                   Forget Password?
                 </Link>
               </div>
 
-              <div className="login-price-div d-flex justify-content-between align-items-center mb-5">
-                <span className="text-white login-price-dollerSign">$</span>
-                <span className="fw-bold text-white login-price">10.00</span>
+              <div className='login-price-div d-flex justify-content-between align-items-center mb-5'>
+                <span className='text-white login-price-dollerSign'>$</span>
+                <span className='fw-bold text-white login-price'>10.00</span>
                 <span></span>
               </div>
 
-              <div className="col-md-12">
+              <div className='col-md-12'>
                 <button
-                  type="button"
-                  className="login-btn"
+                  type='button'
+                  className='login-btn'
                   onClick={submitLogin}
                 >
                   Login
@@ -119,6 +123,13 @@ const Login = () => {
           </GlassBox>
         </div>
       </FullScreenBG>
+
+      {/* <CustomModal
+        show={showModal}
+        close={() => setShowModal(false)}
+        action={() => setShowModal(false)}
+        heading={`Do You Want to Accept ${challengerName}'s Challenge?`}
+      /> */}
     </DefaultLayout>
   );
 };
